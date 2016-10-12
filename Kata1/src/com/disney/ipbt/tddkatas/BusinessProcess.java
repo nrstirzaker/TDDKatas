8fa29ee4290e9ef09ba84ba45fface2871e7c1ce
nrstirzaker <nrstirzaker.techblog@gmail.com>
2016-10-12 11:00:30 +0100

    init commit

---
+++ Kata1/src/com/disney/ipbt/tddkatas/BusinessProcess.java
@@ -0,0 +1,25 @@
+package com.disney.ipbt.tddkatas;
+
+public class BusinessProcess {
+
+	/**
+	 * This method must always return false unless
+	 * 1) Equivalent values on each object are equal
+	 * 		i.e. data1.startDateStatus = data2.startDateStatus & data1.endDateStatus = data2.endDateStatus
+	 * 2) if one data object return a null value for the "start" or "end" then the other data object can 
+	 * 		return 0 for the equivalent method 
+	 * 
+	 * see TruthTable.xlsx in the root of this project
+	 * 
+	 * 1) Create a test class first under test folder and in the package com.disney.ipbt.tddkatas
+	 * 2) Write as many tests as you need need to validate that the method will work 
+	 * 3) No complete the method below so that all you test pass
+	 * 
+	 */
+	
+	
+	public Boolean isDataSimilar(Data data1, Data data2){
+		return null;
+	}
+	
+}
---
+++ Kata1/src/com/disney/ipbt/tddkatas/Data.java
@@ -0,0 +1,25 @@
+package com.disney.ipbt.tddkatas;
+
+public class Data {
+
+	
+	private Long startDateStatus;
+	private Long endDateStatus;
+	
+	public Data(Long startDateStatus, Long endDateStatus) {
+		super();
+		this.startDateStatus = startDateStatus;
+		this.endDateStatus = endDateStatus;
+	}
+	
+	public Long getStartDateStatus() {
+		return startDateStatus;
+	}
+	
+	public Long getEndDateStatus() {
+		return endDateStatus;
+	}
+	
+	
+	
+}
